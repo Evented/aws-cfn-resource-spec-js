2df9d4963a6b4613e0579c5b66a176636d5025ca
Barrett K. Harber <barrett.harber@gmail.com>
2017-08-03 01:52:22 -0400

    Initial

---
+++ scripts/assets.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import https from 'https';
+import zlib from 'zlib';
+
+const url = 'https://d1uauaxba7bl26.cloudfront.net/latest/gzip/CloudFormationResourceSpecification.json';
+
+if (!fs.existsSync('./build')) {
+	fs.mkdirSync('./build')
+}
+
+https.get(url).on('response', (response) => {
+	const {statusCode} = response;
+
+	if (statusCode < 200 || statusCode > 299) {
+		console.error('Invalid HTTP status code');
+		process.exit(1);
+	}
+
+	const output = fs.createWriteStream('./build/spec.json');
+	let res = response;
+
+	switch (response.headers['content-encoding']) {
+		case 'gzip':
+		case 'deflate':
+			res = res.pipe(zlib.createUnzip());
+		default:
+			res.pipe(output);
+	}
+});
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import spec from '../build/spec.json';
+
+export default spec;
